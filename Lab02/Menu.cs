76a2f63c887fc4b6abecc8dffa2220ca6fd3bec4
tachalorah <localhost>
2026-03-26 09:06:09 +0700

    Hoàn thiện menu

--- Lab02/Menu.cs
+++ Lab02/Menu.cs
@@ -16,5 +16,35 @@ namespace Lab02
         {
             InitializeComponent();
         }
+
+        private void Bai01_btn_Click(object sender, EventArgs e)
+        {
+            Bai01 form = new Bai01();
+            form.Show();
+        }
+
+        private void Bai02_btn_Click(object sender, EventArgs e)
+        {
+            Bai02 form = new Bai02();
+            form.Show();
+        }
+
+        private void Bai03_btn_Click(object sender, EventArgs e)
+        {
+            Bai03 form = new Bai03();
+            form.Show();
+        }
+
+        private void Bai04_btn_Click(object sender, EventArgs e)
+        {
+            Bai04 form = new Bai04();
+            form.Show();
+        }
+
+        private void Bai05_btn_Click(object sender, EventArgs e)
+        {
+            Bai05 form = new Bai05();
+            form.Show();
+        }
     }
 }
