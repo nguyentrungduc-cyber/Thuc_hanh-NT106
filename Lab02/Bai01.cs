20cdb25a86fdfd38aee0725141a508e55d101d3e
nguyentrungduc <122720104+nguyentrungduc-cyber@users.noreply.github.com>
2026-03-26 09:42:13 +0700

    Upload bài 01 - ghi và đọc file

--- Lab02/Bai01.cs
+++ Lab02/Bai01.cs
@@ -1,10 +1,41 @@
-namespace Lab02
+﻿namespace Lab02
 {
-    public partial class Bai01 : Form
+    public partial class FrmBai01 : Form
     {
-        public Bai01()
+        public FrmBai01()
         {
             InitializeComponent();
+        }
+
+        private void btnReadFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using FileStream fs = new FileStream("input.txt", FileMode.Open, FileAccess.Read);
+                using StreamReader sr = new StreamReader(fs);
+                {
+                    txtFileContent.Text = sr.ReadToEnd();
+                } // using sẽ tự động đóng StreamReader và FileStream sau khi hoàn thành khối lệnh
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("File not found");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnWriteFile_Click(object sender, EventArgs e)
+        {
+            FileStream sw = new FileStream("output.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            using StreamWriter writer = new StreamWriter(sw);
+            {
+                string content = txtFileContent.Text.ToUpper();
+                writer.Write(content);
+            } // using sẽ tự động đóng StreamWriter và FileStream sau khi hoàn thành khối lệnh
+
         }
     }
 }
