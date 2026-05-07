26aeccab9f0f0c7e7b02773cd0b4dea831138dda
tachalorah <tachalorah@outlook.com>
2026-05-07 10:27:02 +0700

    Bai04 (2/3)

--- Lab04/Bai04.Designer.cs
+++ Lab04/Bai04.Designer.cs
@@ -28,12 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Bai04";
+            webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)webView21).BeginInit();
+            SuspendLayout();
+            // 
+            // webView21
+            // 
+            webView21.AllowExternalDrop = true;
+            webView21.CreationProperties = null;
+            webView21.DefaultBackgroundColor = Color.White;
+            webView21.Location = new Point(0, 47);
+            webView21.Name = "webView21";
+            webView21.Size = new Size(1280, 649);
+            webView21.Source = new Uri("https://www.google.com", UriKind.Absolute);
+            webView21.TabIndex = 0;
+            webView21.ZoomFactor = 1D;
+            webView21.SourceChanged += OnWebViewSourceChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(76, 14);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(886, 27);
+            textBox1.TabIndex = 1;
+            textBox1.KeyPress += textBox1_KeyHandler;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 17);
+            label1.Name = "label1";
+            label1.Size = new Size(58, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Địa chỉ:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(974, 14);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 3;
+            button1.Text = "Truy cập";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(1074, 13);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 4;
+            button2.Text = "Mã nguồn";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(1174, 14);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 5;
+            button3.Text = "Tải về";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // Bai04
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1280, 696);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(webView21);
+            Name = "Bai04";
+            Text = "Bai04";
+            ((System.ComponentModel.ISupportInitialize)webView21).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private TextBox textBox1;
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
--- Lab04/Bai04.cs
+++ Lab04/Bai04.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
 
 namespace Lab04
 {
@@ -15,6 +16,214 @@ namespace Lab04
         public Bai04()
         {
             InitializeComponent();
+            this.Resize += new System.EventHandler(this.Form_Resize);
+        }
+
+        private void Form_Resize(object sender, EventArgs e)
+        {
+            webView21.Size = this.ClientSize - new System.Drawing.Size(webView21.Location);
+            button3.Left = this.ClientSize.Width - button3.Width - 12;
+            button2.Left = this.ClientSize.Width - button3.Width - 6 - button2.Width - 12;
+            button1.Left = this.ClientSize.Width - button3.Width - 6 - button2.Width - 6 - button1.Width - 12;
+
+            textBox1.Width = button1.Left - textBox1.Left - 12;
+        }
+
+        private void textBox1_KeyHandler(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                OpenWebsite();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenWebsite();
+        }
+
+        private void OpenWebsite()
+        {
+            if (webView21 != null && webView21.CoreWebView2 != null && textBox1.Text != String.Empty)
+            {
+                webView21.CoreWebView2.Navigate(textBox1.Text);
+            }
+        }
+
+        private void OnWebViewSourceChanged(object? sender, Microsoft.Web.WebView2.Core.CoreWebView2SourceChangedEventArgs e)
+        {
+            textBox1.Text = webView21.Source.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string currentUrl = webView21.Source.ToString();
+            webView21.CoreWebView2.Navigate($"view-source:{currentUrl}");
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string url = webView21.Source.ToString();
+
+                if (string.IsNullOrEmpty(url))
+                {
+                    MessageBox.Show("Không có website.");
+                    return;
+                }
+
+                // Chọn nơi lưu
+                FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+
+                if (folderDialog.ShowDialog() != DialogResult.OK)
+                    return;
+
+                string saveFolder = folderDialog.SelectedPath;
+
+                using (HttpClient client = new HttpClient())
+                {
+                    // Tải HTML
+                    string html = await client.GetStringAsync(url);
+
+                    HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                    doc.LoadHtml(html);
+
+                    Uri baseUri = new Uri(url);
+
+                    // Tạo thư mục resource
+                    string resourceFolder = Path.Combine(saveFolder, "resources");
+
+                    if (!Directory.Exists(resourceFolder))
+                        Directory.CreateDirectory(resourceFolder);
+
+                    // =========================
+                    // DOWNLOAD IMG
+                    // =========================
+                    var imgNodes = doc.DocumentNode.SelectNodes("//img[@src]");
+
+                    if (imgNodes != null)
+                    {
+                        foreach (var img in imgNodes)
+                        {
+                            string src = img.GetAttributeValue("src", "");
+
+                            if (string.IsNullOrEmpty(src))
+                                continue;
+
+                            try
+                            {
+                                Uri imgUri = new Uri(baseUri, src);
+
+                                string fileName = Path.GetFileName(imgUri.LocalPath);
+
+                                if (string.IsNullOrEmpty(fileName))
+                                    fileName = Guid.NewGuid().ToString() + ".img";
+
+                                string localPath = Path.Combine(resourceFolder, fileName);
+
+                                byte[] data = await client.GetByteArrayAsync(imgUri);
+
+                                File.WriteAllBytes(localPath, data);
+
+                                // đổi src local
+                                img.SetAttributeValue("src", $"resources/{fileName}");
+                            }
+                            catch
+                            {
+
+                            }
+                        }
+                    }
+
+                    // =========================
+                    // DOWNLOAD CSS
+                    // =========================
+                    var cssNodes = doc.DocumentNode.SelectNodes("//link[@href]");
+
+                    if (cssNodes != null)
+                    {
+                        foreach (var css in cssNodes)
+                        {
+                            string href = css.GetAttributeValue("href", "");
+
+                            if (string.IsNullOrEmpty(href))
+                                continue;
+
+                            try
+                            {
+                                Uri cssUri = new Uri(baseUri, href);
+
+                                string fileName = Path.GetFileName(cssUri.LocalPath);
+
+                                if (string.IsNullOrEmpty(fileName))
+                                    fileName = Guid.NewGuid().ToString() + ".css";
+
+                                string localPath = Path.Combine(resourceFolder, fileName);
+
+                                byte[] data = await client.GetByteArrayAsync(cssUri);
+
+                                File.WriteAllBytes(localPath, data);
+
+                                css.SetAttributeValue("href", $"resources/{fileName}");
+                            }
+                            catch
+                            {
+
+                            }
+                        }
+                    }
+
+                    // =========================
+                    // DOWNLOAD JS
+                    // =========================
+                    var jsNodes = doc.DocumentNode.SelectNodes("//script[@src]");
+
+                    if (jsNodes != null)
+                    {
+                        foreach (var js in jsNodes)
+                        {
+                            string src = js.GetAttributeValue("src", "");
+
+                            if (string.IsNullOrEmpty(src))
+                                continue;
+
+                            try
+                            {
+                                Uri jsUri = new Uri(baseUri, src);
+
+                                string fileName = Path.GetFileName(jsUri.LocalPath);
+
+                                if (string.IsNullOrEmpty(fileName))
+                                    fileName = Guid.NewGuid().ToString() + ".js";
+
+                                string localPath = Path.Combine(resourceFolder, fileName);
+
+                                byte[] data = await client.GetByteArrayAsync(jsUri);
+
+                                File.WriteAllBytes(localPath, data);
+
+                                js.SetAttributeValue("src", $"resources/{fileName}");
+                            }
+                            catch
+                            {
+
+                            }
+                        }
+                    }
+
+                    // Lưu index.html
+                    string htmlPath = Path.Combine(saveFolder, "index.html");
+
+                    File.WriteAllText(htmlPath, doc.DocumentNode.OuterHtml, Encoding.UTF8);
+
+                    MessageBox.Show("Tải website thành công!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
